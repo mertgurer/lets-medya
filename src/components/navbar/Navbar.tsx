f15fd3f600791d27e53cae9c3a562d5f6a7aac93
Mert Gürer <mert.gurer@hotmail.com.tr>
2023-08-23 15:49:30 +0300

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/navbar.css";
-import { Link, useFetcher } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/image/logo.jpg";
 import NavbarNavigationButton from "./NavbarNavigationButton";
