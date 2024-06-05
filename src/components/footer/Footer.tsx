477ab303e14ebf7746a5b6b0ec6d347b98fae9d6
Mert Gürer <mert.gurer@hotmail.com.tr>
2024-06-05 18:23:06 +0300

    update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,80 +3,88 @@ import "../../styles/footer.css";
 
 import { MdLocationPin, MdPhone, MdEmail } from "react-icons/md";
 import {
-  FaLinkedinIn,
-  FaInstagram,
-  FaFacebookF,
-  FaYoutube,
+    FaLinkedinIn,
+    FaInstagram,
+    FaFacebookF,
+    FaYoutube,
 } from "react-icons/fa";
 import FooterIcon from "./FooterIcon";
 import { DataContext } from "../../App";
 
 export const Footer = () => {
-  const { isMobile } = useContext(DataContext);
+    const { isMobile } = useContext(DataContext);
 
-  const openMail = () => {
-    const subject = "";
-    const recipient = "info@letsmedya.com";
+    const openMail = () => {
+        const subject = "";
+        const recipient = "info@letsmedya.com";
 
-    const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(
-      subject
-    )}`;
+        const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(
+            subject
+        )}`;
 
-    window.location.href = mailtoLink;
-  };
+        window.location.href = mailtoLink;
+    };
 
-  return (
-    <section className="footer-container">
-      <div className="footer-left">
-        <div className="footer-box">
-          <div className="footer-icon-box">
-            <MdLocationPin color="white" size={isMobile ? 16 : 25} />
-          </div>
-          <p className="footer-text">
-            İçerenköy Mah. Prof. Dr. Ali Nihat Tarlan cad. Süleyman Başkan İş
-            Merkezi No:84 Kat:8 <br /> Ataşehir – İstanbul
-          </p>
-        </div>
-        <div className="footer-box-big">
-          <div className="footer-box">
-            <div className="footer-icon-box">
-              <MdPhone color="white" size={isMobile ? 14 : 18} />
+    return (
+        <section className="footer-container">
+            <div className="footer-left">
+                <div className="footer-box">
+                    <div className="footer-icon-box">
+                        <MdLocationPin
+                            color="white"
+                            size={isMobile ? 16 : 25}
+                        />
+                    </div>
+                    <p className="footer-text">
+                        <p>Sahrayıcedit Mah. Mengi Sok. No: 26</p>
+                        <p>Kat: 2 Ofis No: 828 Kadıköy/İstanbul</p>
+                    </p>
+                </div>
+                <div className="footer-box-big">
+                    <div className="footer-box">
+                        <div className="footer-icon-box">
+                            <MdPhone color="white" size={isMobile ? 14 : 18} />
+                        </div>
+                        <p className="footer-text">0216 912 10 88</p>
+                    </div>
+                    <button
+                        className="footer-box button"
+                        onClick={() => openMail()}
+                    >
+                        <div className="footer-icon-box">
+                            <MdEmail color="white" size={isMobile ? 14 : 18} />
+                        </div>
+                        <p className="footer-text">info@letsmedya.com</p>
+                    </button>
+                </div>
             </div>
-            <p className="footer-text">0216 912 10 88</p>
-          </div>
-          <button className="footer-box button" onClick={() => openMail()}>
-            <div className="footer-icon-box">
-              <MdEmail color="white" size={isMobile ? 14 : 18} />
+            <div className="footer-right">
+                <FooterIcon
+                    logo={FaInstagram}
+                    size={isMobile ? 24 : 28}
+                    url={"https://www.instagram.com/lets.medya/"}
+                />
+                <FooterIcon
+                    logo={FaLinkedinIn}
+                    size={isMobile ? 20 : 24}
+                    url={
+                        "https://www.linkedin.com/company/96047657/admin/feed/posts/"
+                    }
+                />
+                <FooterIcon
+                    logo={FaFacebookF}
+                    size={isMobile ? 20 : 24}
+                    url={"https://www.facebook.com/LETSMEDYA"}
+                />
+                <FooterIcon size={24} url={"https://twitter.com/letsmedya"} />
+                <FooterIcon
+                    logo={FaYoutube}
+                    size={isMobile ? 20 : 24}
+                    url={"https://youtube.com/"}
+                />
             </div>
-            <p className="footer-text">info@letsmedya.com</p>
-          </button>
-        </div>
-      </div>
-      <div className="footer-right">
-        <FooterIcon
-          logo={FaInstagram}
-          size={isMobile ? 24 : 28}
-          url={"https://www.instagram.com/lets.medya/"}
-        />
-        <FooterIcon
-          logo={FaLinkedinIn}
-          size={isMobile ? 20 : 24}
-          url={"https://www.linkedin.com/company/96047657/admin/feed/posts/"}
-        />
-        <FooterIcon
-          logo={FaFacebookF}
-          size={isMobile ? 20 : 24}
-          url={"https://www.facebook.com/LETSMEDYA"}
-        />
-        <FooterIcon size={24} url={"https://twitter.com/letsmedya"} />
-        <FooterIcon
-          logo={FaYoutube}
-          size={isMobile ? 20 : 24}
-          url={"https://youtube.com/"}
-        />
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Footer;
